491abcd24ac29751a872800a191be0dea44715df
davide sbalzer <sbalzerdavide@hotmail.it>
2023-04-07 23:18:02 +0200

    new version

--- js/main2.js
+++ js/main2.js
@@ -1,78 +1,62 @@
 // Extend DayJS with plugin
 dayjs.extend(window.dayjs_plugin_relativeTime);
 
-
 const appV2 = new Vue({
     el: "#app2",
     data:{
-        start: '',
-        time: '',
-        now: '',
-        dayDiff: '',
-        dayDiff2: '',
-        weekDiff: '',
-        weekDiff2: '',
-        diffMore: '',
-        diffMore2: '',
-        term: '',
-        second: '',
-        secondFormat: '',
-        future: '',
-        inputWeek: 0,
-        inputDay: 0,
-        add: '',
-        addedDay: '',
-        startFromEnd: '',
-        inputParto: '',
-        active: 'UM',
+      start: '',
+      time: '',
+      now: '',
+      dayDiff: '',
+      dayDiff2: '',
+      weekDiff: 0,
+      weekDiff2: '',
+      diffMore: '',
+      diffMore2: '',
+      term: '',
+      second: '',
+      secondFormat: '',
+      future: '',
+      inputWeek: 0,
+      inputDay: 0,
+      add: '',
+      addedDay: '',
+      startFromEnd: '',
+      inputParto: '',
+      active: 'UM',
     },
     created(){
-        this.now = dayjs();
+      this.now = dayjs();
     },
     methods:{
-        takeFromStart(){
-            // this.time = dayjs(this.start).format('DD/MM/YYYY');
-            this.time = dayjs(this.start);
-            // this.time = dayjs(this.start).toNow();
-            this.dayDiff = this.now.diff(this.time, 'day');
-            console.log(this.dayDiff, 'day');
-            this.weekDiff = this.now.diff(this.time, 'week');
-            console.log(this.weekDiff, 'week');
-            this.diffMore = this.dayDiff % 7;
-            this.term = dayjs(this.time).add(280, 'day').format('DD/MM/YYYY');
-
-
-            // console.log(this.start.diff(this.now));
-            // const date1 = dayjs('2019-01-25');
-            // const date2 = dayjs('2018-06-05');
-            // const date3 = (date1.diff(date2));
-            // console.log(date3);
-        },
-        go2(){
-            this.secondFormat = dayjs(this.second);
-            this.dayDiff2 = this.secondFormat.diff(this.time, 'day');
-            console.log(this.dayDiff2, 'day');
-            this.weekDiff2 = this.secondFormat.diff(this.time, 'week');
-            console.log(this.weekDiff2, 'week');
-            this.diffMore2 = this.dayDiff2 % 7;
-            this.future = dayjs(this.time).add(280, 'day').format('DD/MM/YYYY');
-
-
-        },
-        go3(){
-            this.add = parseInt(this.inputDay) + (parseInt(this.inputWeek) * 7 );
-            this.addedDay = dayjs(this.time).add(this.add, 'day').format('DD/MM/YYYY');
-        },
-        takefromEnd(){
-            this.time = dayjs(this.inputParto).subtract(280, 'day');
-            this.dayDiff = this.now.diff(this.time, 'day');
-            console.log(this.dayDiff, 'day');
-            this.weekDiff = this.now.diff(this.time, 'week');
-            console.log(this.weekDiff, 'week');
-            this.diffMore = this.dayDiff % 7;
-            this.term = dayjs(this.time).add(280, 'day').format('DD/MM/YYYY');
-
-
-        }
+      refresh(){
+        this.time = "";
+        this.dayDiff = "";
+        this.weekDiff = 0;
+        this.term = "";
+      },
+      takeFromStart(){
+        this.time = dayjs(this.start);
+        this.dayDiff = this.now.diff(this.time, 'day');
+        this.weekDiff = this.now.diff(this.time, 'week');
+        this.term = dayjs(this.time).add(280, 'day').format('DD/MM/YYYY');
+      },
+      takefromEnd(){
+        this.time = dayjs(this.inputParto).subtract(280, 'day');
+        this.dayDiff = this.now.diff(this.time, 'day');
+        this.weekDiff = this.now.diff(this.time, 'week');
+        this.term = dayjs(this.time).add(280, 'day').format('DD/MM/YYYY');
+      },
+      go2(){
+        this.secondFormat = dayjs(this.second);
+        this.dayDiff2 = this.secondFormat.diff(this.time, 'day');
+        this.weekDiff2 = this.secondFormat.diff(this.time, 'week');
+        this.diffMore2 = this.dayDiff2 % 7;
+        this.future = dayjs(this.time).add(280, 'day').format('DD/MM/YYYY');
+      },
+      go3(){
+        this.add = parseInt(this.inputDay) + (parseInt(this.inputWeek) * 7 );
+        this.addedDay = dayjs(this.time).add(this.add, 'day').format('DD/MM/YYYY');
+      },
     }
 })
